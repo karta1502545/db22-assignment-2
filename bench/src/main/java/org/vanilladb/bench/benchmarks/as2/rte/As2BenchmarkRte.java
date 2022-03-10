6a9e5e733f19034f1721629ae724237858bda8b5
Yuxuan <xuan19970825@gmail.com>
2022-03-10 14:48:28 +0800

    Initial commit

---
+++ bench/src/main/java/org/vanilladb/bench/benchmarks/as2/rte/As2BenchmarkRte.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright 2016, 2018 vanilladb.org contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.vanilladb.bench.benchmarks.as2.rte;
+
+import org.vanilladb.bench.StatisticMgr;
+import org.vanilladb.bench.benchmarks.as2.As2BenchTransactionType;
+import org.vanilladb.bench.remote.SutConnection;
+import org.vanilladb.bench.rte.RemoteTerminalEmulator;
+
+public class As2BenchmarkRte extends RemoteTerminalEmulator<As2BenchTransactionType> {
+	
+	private As2BenchmarkTxExecutor executor;
+
+	public As2BenchmarkRte(SutConnection conn, StatisticMgr statMgr, long sleepTime) {
+		super(conn, statMgr, sleepTime);
+		executor = new As2BenchmarkTxExecutor(new As2ReadItemParamGen());
+	}
+	
+	protected As2BenchTransactionType getNextTxType() {
+		return As2BenchTransactionType.READ_ITEM;
+	}
+	
+	protected As2BenchmarkTxExecutor getTxExeutor(As2BenchTransactionType type) {
+		return executor;
+	}
+}
